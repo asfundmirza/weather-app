624dc2e4ce05c9bc26ceb742887ec3dad5233723
asfund <asfundmirzaj@gmail.com>
2023-09-29 11:31:39 +0500

    added loading effect

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
   const temperatureInCelsius = (temperatureInFahrenheit - 273.15).toFixed(2);
   return (
     <div className="flex justify-center items-center min-h-[100vh] min-w-[100vw]">
-      <div className="flex  p-14   w-[600px]  m-3 rounded-lg  bg-black">
+      <div className="flex  p-14   w-[600px]  m-3 rounded-lg  bg-gradient-to-r from-indigo-800 via-purple-600 to-cyan-200">
         <div className="flex-1 flex-col   ">
           <form
             onSubmit={handleSubmit}
@@ -56,10 +56,10 @@ export default function Home() {
               type="text"
               placeholder="City Name"
               onChange={(e) => setSearchValue(e.target.value)}
-              className="p-4 rounded-3xl text-lg outline-none w-full"
+              className="p-4 rounded-full text-lg outline-none w-full "
             />
             <button
-              className="flex py-4 px-6 md:px-4 bg-slate-100 rounded-3xl justify-center text-center"
+              className="flex py-4 px-6 md:px-4 bg-black text-white rounded-full justify-center text-center"
               type="submit"
             >
               Search
@@ -73,47 +73,61 @@ export default function Home() {
             </div>
           ) : (
             <>
-              {weatherData?.weather?.length > 0 && (
-                <div className="flex md:flex-row flex-col justify-between items-center">
-                  <div className="relative">
-                    <img
-                      src={`https://openweathermap.org/img/wn/${weatherData.weather[0]?.icon}@2x.png`}
-                      alt="icon"
-                      width={200}
-                      height={200}
-                      className="flex"
-                    />
-                    <p className="absolute text-white left-0 right-0  bottom-6 text-center">
-                      {weatherData?.weather[0]?.description}
-                    </p>
-                  </div>
-                  <div className="hidden md:block text-white md:text-6xl text-5xl font-semibold relative">
-                    {temperatureInCelsius}
-                    <span className="text-sm absolute top-0 right-[-2]">
-                      °C
-                    </span>
-                  </div>
-
-                  {/* for mobile */}
-
-                  <div className="md:hidden flex justify-between items-center w-full  ">
-                    <div className="text-white  text-2xl font-semibold">
-                      {weatherData?.name}
+              {weatherData && weatherData?.weather?.length > 0 ? (
+                <>
+                  <div className="flex md:flex-row flex-col justify-between items-center">
+                    <div className="relative">
+                      <img
+                        src={`https://openweathermap.org/img/wn/${weatherData.weather[0]?.icon}@2x.png`}
+                        alt="icon"
+                        width={200}
+                        height={200}
+                        className="flex"
+                      />
+                      <p className="absolute text-white left-0 right-0  bottom-6 text-center">
+                        {weatherData?.weather[0]?.description}
+                      </p>
                     </div>
-                    <div className=" text-white  text-3xl font-semibold relative">
+                    <div className="hidden md:block text-white md:text-6xl text-5xl font-semibold relative">
                       {temperatureInCelsius}
                       <span className="text-sm absolute top-0 right-[-2]">
                         °C
                       </span>
                     </div>
+
+                    {/* for mobile */}
+
+                    <div className="md:hidden flex justify-between items-center w-full  ">
+                      <div className="text-white  text-2xl font-semibold">
+                        {weatherData?.name}
+                      </div>
+                      <div className=" text-white  text-3xl font-semibold relative">
+                        {temperatureInCelsius}
+                        <span className="text-sm absolute top-0 right-[-2]">
+                          °C
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="hidden md:block mt-3 text-center">
+                    <div className="text-white md:text-6xl text-2xl font-semibold">
+                      {weatherData?.name}
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <div className="flex justify-center items-center mt-12">
+                  <div
+                    className="  inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent  align-[-0.125em] text-blue-500 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    role="status"
+                  >
+                    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                      Loading...
+                    </span>
                   </div>
                 </div>
               )}
-              <div className="hidden md:block mt-3 text-center">
-                <div className="text-white md:text-6xl text-2xl font-semibold">
-                  {weatherData?.name}
-                </div>
-              </div>
             </>
           )}
         </div>
